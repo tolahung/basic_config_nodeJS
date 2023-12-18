4bb5780b2742f6513867b2a2fcc1a5d0c443cda6
tolahung <idoky19@gmail.com>
2023-12-18 10:51:05 +0700

    upadate config

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import  express  from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./routes/web"
+require('dotenv').config();
+
+
+let app = express();
+
+
+//config app 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+viewEngine(app);
+initWebRoutes(app);
+let port = process.env.PORT || 6969;
+//PORT === undifined => port = 6969
+
+app.listen(port, () => {
+    console.log(`app listening on http://localhost:${port}`);
+})
