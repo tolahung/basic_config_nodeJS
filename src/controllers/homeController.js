442a44b37e1f2ac0f0d9b42ae1919d905dc329c4
tolahung <idoky19@gmail.com>
2023-12-21 00:43:53 +0700

    #21-connect database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,15 @@
 
-
-class HomeController{
-    getHomePage(req,res){
-        return res.render('homepages');
+import db from '../models/index';
+class HomeController {
+    async getHomePage(req, res) {
+        try {
+            let data = await db.User.findAll(); // Ten User là từ models/user
+            return res.render('homepages',{
+                data:data
+            });
+        } catch (e) {
+            console.log(e);
+        }
     }
 }
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web"
+import connectDB from './config/connectDB';
 require('dotenv').config();
 
 
@@ -16,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 viewEngine(app);
 initWebRoutes(app);
+connectDB();
+
 let port = process.env.PORT || 6969;
 //PORT === undifined => port = 6969
 
